ae4aac94255e889473211639fcf857e0292e3674
ILykSonic <147686259+ILykSonic@users.noreply.github.com>
2024-06-14 11:05:37 -0500

    Add files via upload

--- ingest.py
+++ ingest.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python3
-import os
 import glob
-from typing import List
 from multiprocessing import Pool
 from tqdm import tqdm
-
-from langchain.document_loaders import (
+import torch
+import os
+import warnings
+import pytesseract
+from PIL import Image
+import io
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from constants import CHROMA_SETTINGS
+from sympy import sympify
+from langchain_community.document_loaders import (
     CSVLoader,
     EverNoteLoader,
     PyMuPDFLoader,
@@ -19,47 +26,37 @@ from langchain.document_loaders import (
     UnstructuredWordDocumentLoader,
 )
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.docstore.document import Document
-from constants import CHROMA_SETTINGS
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# Load environment variables
 persist_directory = os.environ.get('PERSIST_DIRECTORY', 'db')
 source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME', 'all-MiniLM-L6-v2')
-chunk_size = 500
-chunk_overlap = 50
+chunk_size = 1000
+chunk_overlap = 100
 
 # Custom document loaders
 class MyElmLoader(UnstructuredEmailLoader):
-    """Wrapper to fallback to text/plain when default does not work"""
-
-    def load(self) -> List[Document]:
-        """Wrapper adding fallback for elm without html"""
+    def load(self):
         try:
             try:
                 doc = UnstructuredEmailLoader.load(self)
             except ValueError as e:
                 if 'text/html content not found in email' in str(e):
-                    # Try plain text
-                    self.unstructured_kwargs["content_source"]="text/plain"
+                    self.unstructured_kwargs["content_source"] = "text/plain"
                     doc = UnstructuredEmailLoader.load(self)
                 else:
                     raise
         except Exception as e:
-            # Add file_path to exception message
             raise type(e)(f"{self.file_path}: {e}") from e
-
         return doc
-
 
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
     ".csv": (CSVLoader, {}),
-    # ".docx": (Docx2txtLoader, {}),
     ".doc": (UnstructuredWordDocumentLoader, {}),
     ".docx": (UnstructuredWordDocumentLoader, {}),
     ".enex": (EverNoteLoader, {}),
@@ -72,23 +69,58 @@ LOADER_MAPPING = {
     ".ppt": (UnstructuredPowerPointLoader, {}),
     ".pptx": (UnstructuredPowerPointLoader, {}),
     ".txt": (TextLoader, {"encoding": "utf8"}),
-    # Add more mappings for other file extensions and loaders as needed
 }
 
+def extract_text_with_ocr(page):
+    text = page.get_text("text")
+    for img in page.get_images(full=True):
+        xref = img[0]
+        base_image = page.document.extract_image(xref)
+        image_bytes = base_image["image"]
+        image = Image.open(io.BytesIO(image_bytes))
+        ocr_text = pytesseract.image_to_string(image)
+        text += f"\n[Image OCR]:\n{ocr_text}\n"
+    return text
 
-def load_single_document(file_path: str) -> List[Document]:
+def extract_equations(text):
+    equations = []
+    for line in text.split('\n'):
+        try:
+            equation = sympify(line)
+            equations.append(str(equation))
+        except:
+            continue
+    return '\n'.join(equations)
+
+def process_document(doc):
+    if isinstance(doc, list):
+        processed_docs = []
+        for sub_doc in doc:
+            if hasattr(sub_doc, 'pages'):
+                for page in sub_doc.pages:
+                    page_text = extract_text_with_ocr(page)
+                    equations = extract_equations(page_text)
+                    sub_doc.text += page_text + "\n" + equations
+            processed_docs.append(sub_doc)
+        return processed_docs
+    else:
+        if hasattr(doc, 'pages'):
+            for page in doc.pages:
+                page_text = extract_text_with_ocr(page)
+                equations = extract_equations(page_text)
+                doc.text += page_text + "\n" + equations
+        return [doc]
+
+def load_single_document(file_path):
     ext = "." + file_path.rsplit(".", 1)[-1]
     if ext in LOADER_MAPPING:
         loader_class, loader_args = LOADER_MAPPING[ext]
         loader = loader_class(file_path, **loader_args)
-        return loader.load()
-
+        docs = loader.load()
+        return process_document(docs)
     raise ValueError(f"Unsupported file extension '{ext}'")
 
-def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
-    """
-    Loads all documents from the source documents directory, ignoring specified files
-    """
+def load_documents(source_dir, ignored_files=[]):
     all_files = []
     for ext in LOADER_MAPPING:
         all_files.extend(
@@ -100,15 +132,13 @@ def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Docum
         results = []
         with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
             for i, docs in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
+                print(f"Processing file: {filtered_files[i]}")
                 results.extend(docs)
                 pbar.update()
 
     return results
 
-def process_documents(ignored_files: List[str] = []) -> List[Document]:
-    """
-    Load documents and split in chunks
-    """
+def process_documents(ignored_files=[]):
     print(f"Loading documents from {source_directory}")
     documents = load_documents(source_directory, ignored_files)
     if not documents:
@@ -120,25 +150,13 @@ def process_documents(ignored_files: List[str] = []) -> List[Document]:
     print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
     return texts
 
-def does_vectorstore_exist(persist_directory: str) -> bool:
-    """
-    Checks if vectorstore exists
-    """
-    if os.path.exists(os.path.join(persist_directory, 'index')):
-        if os.path.exists(os.path.join(persist_directory, 'chroma-collections.parquet')) and os.path.exists(os.path.join(persist_directory, 'chroma-embeddings.parquet')):
-            list_index_files = glob.glob(os.path.join(persist_directory, 'index/*.bin'))
-            list_index_files += glob.glob(os.path.join(persist_directory, 'index/*.pkl'))
-            # At least 3 documents are needed in a working vectorstore
-            if len(list_index_files) > 3:
-                return True
-    return False
+def does_vectorstore_exist(persist_directory):
+    index_files = glob.glob(os.path.join(persist_directory, 'index/*'))
+    return os.path.exists(os.path.join(persist_directory, 'chroma-collections.parquet')) and os.path.exists(os.path.join(persist_directory, 'chroma-embeddings.parquet')) and len(index_files) > 3
 
 def main():
-    # Create embeddings
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-
     if does_vectorstore_exist(persist_directory):
-        # Update and store locally vectorstore
         print(f"Appending to existing vectorstore at {persist_directory}")
         db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
         collection = db.get()
@@ -146,16 +164,19 @@ def main():
         print(f"Creating embeddings. May take some minutes...")
         db.add_documents(texts)
     else:
-        # Create and store locally vectorstore
         print("Creating new vectorstore")
         texts = process_documents()
         print(f"Creating embeddings. May take some minutes...")
-        db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory)
-    db.persist()
-    db = None
+        batch_size = 5000
+        text_batches = [texts[i:i + batch_size] for i in range(0, len(texts), batch_size)]
 
-    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
-
+        for i, text_batch in enumerate(text_batches):
+            print(f"Processing batch {i + 1} of {len(text_batches)}")
+            if i == 0:
+                db = Chroma.from_documents(text_batch, embeddings, persist_directory=persist_directory)
+            else:
+                db.add_documents(text_batch)
+        print("Data ingestion has finished, you can ask the chatbot questions regarding your files.")
 
 if __name__ == "__main__":
     main()
--- privateGPT.py
+++ privateGPT.py
@@ -1,23 +1,41 @@
-#!/usr/bin/env python3
 from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.vectorstores import Chroma
-from langchain.llms import Ollama
-import chromadb
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
 import os
 import argparse
 import time
+from yaspin import yaspin
+import torch
+import warnings
 
-model = os.environ.get("MODEL", "mistral")
-# For embeddings model, the example uses a sentence-transformers model
-# https://www.sbert.net/docs/pretrained_models.html 
-# "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster and still offers good quality."
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_101"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # specify which GPU(s) to be used
+print("GPU is being used:", torch.cuda.is_available())
+
+model = os.environ.get("MODEL", "llama3")  # Change each time you switch models
+# https://www.sbert.net/docs/pretrained_models.html
+
+#os.environ["EMBEDDINGS_MODEL_NAME"] = "multi-qa-mpnet-base-cos-v1"
+
+# These three get information from chroma. If it cannot be obtained it will default to the information after the comma
+# You can set an Embedding model yourself, or use the MiniLM-L6. I suggest the LM-L6, its MTEB benchmark is great
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME", "all-MiniLM-L6-v2")
+# If you set PD, you can choose where you store your chroma database. Otherwise, it will save to a folder titled "db"
 persist_directory = os.environ.get("PERSIST_DIRECTORY", "db")
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+# This determines the number of chunks the chatbot tries to receive from the database.
+# Higher = Slower & More Accurate/More Detailed  Lower = Faster & Less Accurate/Less Detailed
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 10000))
 
 from constants import CHROMA_SETTINGS
+
 
 def main():
     # Parse the command line arguments
@@ -27,39 +45,60 @@ def main():
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
 
     llm = Ollama(model=model, callbacks=callbacks)
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever,
+                                     return_source_documents=not args.hide_source)
     # Interactive questions and answers
     while True:
-        query = input("\nEnter a query: ")
+        query = input("\nEnter a query, or type 'finished' when done asking questions. ")
         if query == "exit":
+            break
+        if query.lower() == "finished":
             break
         if query.strip() == "":
             continue
 
-        # Get the answer from the chain
-        start = time.time()
-        res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-        end = time.time()
+        with yaspin(text="Generating answer...", color="green") as spinner:
+            start = time.time()
+            res = qa.invoke(query)
+            end = time.time()
+            spinner.ok("✅ ")
 
+            # Calculate the time it took to generate the answer
+            duration = end - start
+            # Print the time it took to generate the answer
+            print(f"\n> Time taken to generate the answer: {duration} seconds")
+
+        # Get the answer from the chain
+        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+
+        print("\n" + "-" * 50 + "\n")
         # Print the result
         print("\n\n> Question:")
         print(query)
+        print("\n> Answer:")
         print(answer)
 
+        print("\n" + "-" * 50 + "\n")
+
         # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        if not args.hide_source:
+            print("\n> Relevant Sources:")
+            for document in docs[:2]:  # Change this line depending on how many sources you want.
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+        print("\n" + "-" * 50 + "\n")
+
+
+
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
+    parser = argparse.ArgumentParser(
+        description='privateGPT: Ask questions to your documents without an internet connection, '
+                    'using the power of LLMs.')
     parser.add_argument("--hide-source", "-S", action='store_true',
                         help='Use this flag to disable printing of source documents used for answers.')
 
