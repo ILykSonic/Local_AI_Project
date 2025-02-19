a4b7cf3d704b5aa0e1517184ca082a8d3252e2e6
ILykSonic <147686259+ILykSonic@users.noreply.github.com>
2025-02-19 00:08:47 -0600

    Create py.java

---
+++ py.java
@@ -0,0 +1,36 @@
+/*
+ * Money
+ * 
+ * 1.0
+ *
+ * Feb. 18th, 2025
+ * 
+ * Created for UAH CS321 Spring 2025 Team 2
+ * Authors: Nilan
+ */
+
+package uah.squad.starclick_meadow;
+
+/**
+ * tracks player's money
+ */
+public class Money {
+    private int money = 0; 
+
+    public Money() {
+        // initializes money
+    }
+
+    public void addMoney(int amount) {
+        // adds money to balance
+    }
+
+    public void removeMoney(int amount) {
+        // removes money from balance
+    }
+
+    public int getMoney() {
+        // returns balance
+        return 0;
+    }
+}
